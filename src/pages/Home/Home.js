4753f90d5a96ae48efe1552d9dd9de3b6d940f13
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-08-03 12:33:04 -0300

    busca na api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,125 +1,51 @@
 import React, { Component } from "react";
 
+import api from "../../services/api";
+
 import { ProductList } from "./styles";
 
 import { MdAddShoppingCart } from "react-icons/md";
 
+import { formatPrice } from "../../util/format";
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get("products");
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
   render() {
+    const { products } = this.state;
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />3
+              </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://45582.cdn.simplo7.net/static/45582/sku/adidas-tenis-adidas-kanadia-tr7-1591017425972.jpg"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,00</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" />3
-            </div>
-
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
